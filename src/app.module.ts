d1fd5d8b71ebb4c913017f532703d54a1c5179ce
yanceyy <yanceyy@users.noreply.github.com>
2023-12-13 14:11:41 +1000

    feat: add Winston logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,24 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import process from "process";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { WinstonModule } from "nest-winston";
+import loggingConfig from "./config/logging.config";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [],
+      load: [loggingConfig],
       cache: true,
       envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+    WinstonModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        ...(await configService.get("logging")),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
---
+++ src/config/logging.config.ts
@@ -0,0 +1,37 @@
+import { registerAs } from "@nestjs/config";
+import winston from "winston";
+import { utilities as nestWinstonModuleUtilities } from "nest-winston";
+import type { ConsoleTransportInstance, FileTransportInstance } from "winston/lib/winston/transports";
+
+export default registerAs("logging", () => {
+  const transports: (FileTransportInstance | ConsoleTransportInstance)[] = [
+    // File transports
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ];
+
+  if (process.env.NODE_ENV !== "production") {
+    // Only add Console transport in non-production environments
+    transports.push(
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.ms(),
+          nestWinstonModuleUtilities.format.nestLike("admin-board", {
+            colors: true,
+            prettyPrint: true,
+          }),
+        ),
+      }),
+    );
+  }
+
+  return {
+    level: process.env.LOG_LEVEL || "debug",
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json(),
+    ),
+    transports: transports,
+  };
+});
