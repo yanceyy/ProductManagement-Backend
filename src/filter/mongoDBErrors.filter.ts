7c1a45c47cd75213ea4c295fbb6f02788b795917
yanceyy <yanceyy@users.noreply.github.com>
2023-12-14 14:49:57 +1000

    feat: downgrade mongoDB version to keep the same as the mongoose's current used version

--- src/filter/mongoDBErrors.filter.ts
+++ src/filter/mongoDBErrors.filter.ts
@@ -5,39 +5,35 @@ import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { Logger } from "winston";
 import type { Response } from "express";
 
-@Catch(Error)
+// the version of the current project's mongodb and the mongoose's mongodb must be same to be able to catch error
+@Catch(MongoServerError)
 @Injectable()
 export class MongoErrorFilter implements ExceptionFilter {
   @Inject(WINSTON_MODULE_PROVIDER)
   private readonly logger: Logger;
   catch(exception: MongoServerError, host: ArgumentsHost) {
-    /* since exception.constructor === MongoServerError "mongodb" is false
-      can check an instance type by using instanceof
-     */
-    if (exception.name === "MongoServerError") {
-      const ctx = host.switchToHttp();
-      const response = ctx.getResponse<Response>();
-      let message: string, statusCode: number;
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    let message: string, statusCode: number;
 
-      switch (exception.code) {
-        case 11000: {
-          message = "username already Exists";
-          statusCode = 400;
-          break;
-        }
-        default: {
-          this.logger.error("mongodbDB error", exception.message);
-          message = "DB error";
-          statusCode = 500;
-          break;
-        }
+    switch (exception.code) {
+      case 11000: {
+        message = "username already Exists";
+        statusCode = 400;
+        break;
       }
-      this.logger.error("DB error", exception);
-      response.status(statusCode).json({
-        message: message,
-        error: "Bad Request",
-        statusCode,
-      });
+      default: {
+        this.logger.error("mongodbDB error", exception.message);
+        message = "DB error";
+        statusCode = 500;
+        break;
+      }
     }
+
+    response.status(statusCode).json({
+      message: message,
+      error: "Bad Request",
+      statusCode,
+    });
   }
 }
