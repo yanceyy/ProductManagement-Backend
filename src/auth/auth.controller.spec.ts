42407ca9bfbfdd0a242f7fb81e196ce1eb4f597d
yanceyy <yanceyy@users.noreply.github.com>
2023-12-24 20:53:29 +1000

    test: auth controllers

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,40 +1,56 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import {
-  closeInMemoryDatabaseConnection,
-  createInMemoryDatabaseModule,
-} from "../test/helper/inMemoryDatabase.mock.module";
-import { MongooseModule } from "@nestjs/mongoose";
-import { User, UserSchema } from "../schemas/user.schema";
-import { UserService } from "../user/user.service";
-import { createJWTModule } from "../test/helper/jwt.mock.module";
 import { createWinstonModule } from "../test/helper/winston.mock.module";
-import { RoleService } from "../role/role.service";
-import { Role, RoleSchema } from "../schemas/role.schema";
+import { LoginDto } from "./dto/login.dto";
 
 describe("AuthController", () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        createInMemoryDatabaseModule(),
-        createJWTModule(),
-        createWinstonModule(),
-        MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }]),
-        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-      ],
+      imports: [createWinstonModule()],
       controllers: [AuthController],
-      providers: [AuthService, UserService, RoleService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  afterEach(async () => await closeInMemoryDatabaseConnection());
+  describe("login", () => {
+    it("should return a token on successful login", async () => {
+      const loginDto: LoginDto = {
+        username: "admin",
+        password: "admin",
+      };
 
-  it("should be defined", () => {
-    expect(controller).toBeDefined();
+      const mockResponse = { token: "some-token", username: "admin", id: "1", role: ["admin"] };
+      jest.spyOn(authService, "login").mockResolvedValue(mockResponse);
+
+      const result = await controller.login(loginDto);
+
+      expect(result).toEqual(mockResponse);
+      expect(authService.login).toHaveBeenCalledWith(loginDto);
+    });
+
+    it("should throw an error for invalid credentials", async () => {
+      const loginDto: LoginDto = {
+        username: "admin",
+        password: "admin",
+      };
+
+      jest.spyOn(authService, "login").mockRejectedValue(new Error("Invalid credentials"));
+
+      await expect(controller.login(loginDto)).rejects.toThrow("Invalid credentials");
+    });
   });
 });
